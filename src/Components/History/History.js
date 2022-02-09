2090fa7f643af77d85e265f6349e4127c4113085
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-02-10 00:49:04 +0600

    maked some structure

---
+++ src/Components/History/History.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const History = () => {
+    return (
+        <section className="history">
+            <div className="container">
+
+            </div>
+        </section>
+    );
+};
+
+export default History;
