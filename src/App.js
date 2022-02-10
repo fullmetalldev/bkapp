7a8c13ae7f292646be523838b14c1a9f7084a589
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-02-10 13:27:14 +0600

    writed header-up chapter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import {Routes, Route} from "react-router-dom"
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import History from "./Components/History/History";
 
 function App() {
     return (
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import "./headerStyles.css";
+import tg from "./imgs/telegram.svg";
+import call from "./imgs/call.svg";
+import bag from "./imgs/bag.svg";
 
 const Header = () => {
     return (
-        <div>
+        <header className="header">
+            <section className="header__up">
+                <div className="headerContainer">
+                    <nav className="header__up-navbar">
+                        <h3 className="header__up-title">Производство изделий из пластмассы</h3>
+                        <ul className="header__up-right">
+                            <li className="header__up-right_telegram">
+                                <img src={tg} alt="tg"/>
+                                <span>Напишите нам</span>
+                            </li>
+                            <li className="header__up-right_call">
+                                <img src={call} alt="call"/>
+                                <span>+ 7 (383)-377-77-32</span>
+                            </li>
+                            <li className="header__up-right_btn">
+                                <button className="header__up-right_button">Заказать звонок</button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>
+            <section className="header__down">
+                <div className="headerContainer">
 
-        </div>
+                </div>
+            </section>
+        </header>
     );
 };
 
