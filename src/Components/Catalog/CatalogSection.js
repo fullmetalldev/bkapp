575378857c8c977bd46672e63634a011a574aeca
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-02-13 18:01:44 +0600

    load last pull

--- src/Components/Catalog/CatalogSection.js
+++ src/Components/Catalog/CatalogSection.js
@@ -3,6 +3,9 @@ import "./catalog.css";
 import comp1 from "./imgs/complect1.png";
 import comp2 from "./imgs/complect2.png";
 import comp3 from "./imgs/complect3.png";
+import bfrPrd1 from "./imgs/beforePrd2.png";
+import bfrPrd2 from "./imgs/beforePrd1.png";
+import CatalogProducts from "./CatalogProducts/CatalogProducts";
 
 const CatalogSection = () => {
     return (
@@ -52,6 +55,42 @@ const CatalogSection = () => {
                             </h2>
                         </div>
                     </div>
+
+                    <div className="sectionCatalog__beforeProductsRow">
+
+                        <div className="sectionCatalog__beforeProductsRow-card">
+                            <h2 className="sectionCatalog__beforeProductsRow-card_title">Комплектующие для окон ПВХ</h2>
+                            <div className="sectionCatalog__beforeProductsRow-card_ul">
+                                <div className="sectionCatalog__beforeProductsRow-card_left">
+                                    <span>Фальцевые вкладыши</span>
+                                    <span>Заглушки для подоконника</span>
+                                    <span>Крепления для подоконника</span>
+                                </div>
+                                <div className="sectionCatalog__beforeProductsRow-card_right">
+                                    <span className="sectionCatalog__beforeProductsRow-card_red">Дренажные колпачки</span>
+                                    <span>Ручки балконные</span>
+                                </div>
+                            </div>
+                            <img src={bfrPrd1} alt="beforeCard1"/>
+
+                        </div>
+                        <div className="sectionCatalog__beforeProductsRow-card">
+                            <h2 className="sectionCatalog__beforeProductsRow-card_title">Комплектующие для натяжных потолков
+                            </h2>
+                            <div className="sectionCatalog__beforeProductsRow-card_ul">
+                                <div className="sectionCatalog__beforeProductsRow-card_left">
+                                    <span>Установочные кольца</span>
+                                    <span>Протекторные кольца</span>
+                                </div>
+                            </div>
+                            <img src={bfrPrd2} alt="beforeCard2"/>
+
+                        </div>
+
+                    </div>
+
+                    <CatalogProducts/>
+
                 </section>
 
             </div>
