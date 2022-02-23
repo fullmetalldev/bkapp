73b0e8e8b36f8b3f2f7783b721c47ca86eee959c
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-02-23 14:36:34 +0600

    1600px max

--- src/Components/OrderForm/OrderForm.js
+++ src/Components/OrderForm/OrderForm.js
@@ -3,32 +3,34 @@ import back from "../../Pages/Catalog/imgs/BackGroundForm.png";
 
 const OrderForm = () => {
     return (
-        <form className="sectionCatalog__form">
+        <div className="container">
+            <form className="sectionCatalog__form">
 
-            <div className="sectionCatalog__form-left">
-                <h2 className="sectionCatalog__form-title">
-                    Заказать образец
-                </h2>
-                <label className="sectionCatalog__form-label">
-                    <input placeholder="Ваше имя" type="text" required/>
-                    <input placeholder="Ваш телефон" type="number" required/>
-                    <input placeholder="Ваш e-mail" type="email" required/>
-                </label>
-                <div className="sectionCatalog__form-btnAndInfo">
-                    <div className="infoAndCheckbox">
-                        <input className="infoAndCheckbox_checkbox" type="checkbox"/>
-                        <h4 className="infoAndCheckbox_title">Ваши данные защищены и используются
-                            только <br/> для связи с вами</h4>
+                <div className="sectionCatalog__form-left">
+                    <h2 className="sectionCatalog__form-title">
+                        Заказать образец
+                    </h2>
+                    <label className="sectionCatalog__form-label">
+                        <input placeholder="Ваше имя" type="text" required/>
+                        <input placeholder="Ваш телефон" type="number" required/>
+                        <input placeholder="Ваш e-mail" type="email" required/>
+                    </label>
+                    <div className="sectionCatalog__form-btnAndInfo">
+                        <div className="infoAndCheckbox">
+                            <input className="infoAndCheckbox_checkbox" type="checkbox"/>
+                            <h4 className="infoAndCheckbox_title">Ваши данные защищены и используются
+                                только <br/> для связи с вами</h4>
+                        </div>
+                        <button className="sectionCatalog__form-btnAndInfo_button">Отправить</button>
                     </div>
-                    <button className="sectionCatalog__form-btnAndInfo_button">Отправить</button>
                 </div>
-            </div>
 
-            <div className="sectionCatalog__form-right">
-                <img className="sectionCatalog__form-img" src={back} alt="back"/>
-            </div>
+                <div className="sectionCatalog__form-right">
+                    <img className="sectionCatalog__form-img" src={back} alt="back"/>
+                </div>
 
-        </form>
+            </form>
+        </div>
     );
 };
 
