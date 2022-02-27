8e772b2465e0bb8770d139481d18fac0f2ad3563
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-02-27 23:34:53 +0600

    thanks added

--- src/Pages/Form/Form.js
+++ src/Pages/Form/Form.js
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./FormStyles.css";
 
 const Form = ({form, setForm}) => {
+    const [content, setContent] = useState('form');
+    const contentHandler = () => {
+        return setContent('thanks')
+    };
     return (
         <>
-        <div className="formBlackLayer" onClick={()=> setForm(!form)}>
-
-        </div>
-
-        <div className="formBack">
-            <div className="formBack-layer">
+            <div className="formBlackLayer" onClick={() => setForm(!form)}>
 
             </div>
+            {content === 'form'
+                ? <div className="formBack">
+                    <div className="formBack-layer">
 
-            <form className="form">
+                    </div>
+
+                    <form className="form">
 
 
+                        <h2>Напишите нам</h2>
 
-                <h2>Напишите нам</h2>
+                        <input className="form_inp" type="number" placeholder="Телефон"/>
+                        <input className="form_inp" type="text" placeholder="Имя"/>
 
-                <input className="form_inp" type="number" placeholder="Телефон"/>
-                <input className="form_inp" type="text" placeholder="Имя"/>
+                        <div className="checkbox">
+                            <input type="checkbox" checked={true}/>
+                            <span>Ваши данные защищены и используются только для <br/> связи с вами</span>
+                        </div>
 
-                <div className="checkbox">
-                    <input type="checkbox" checked={true}/>
-                    <span>Ваши данные защищены и используются только для <br/> связи с вами</span>
+                        <button onClick={() => contentHandler()} className="form_button">Отправить</button>
+
+                    </form>
+
                 </div>
+                : <div className="thanks">
+                        <div className="thanks__layer">
 
-                <button className="form_button">Отправить</button>
-
-            </form>
-
-        </div>
+                        </div>
+                        <div className="thanks__content">
+                            <div onClick={() => setForm(!form)} className="thanks-close">
+                                X
+                            </div>
+                            <h2>Спасибо!</h2>
+                            <p>Ваше сообщение отправлено!
+                                Менеджер свяжется с вами в ближайшее время.</p>
+                        </div>
+                    </div>
+            }
         </>
     );
 };
